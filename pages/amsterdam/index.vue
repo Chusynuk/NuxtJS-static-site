185c69d3b03f7307ced0a6892b2d6bad632b1d8e
Chusynuk <recycled_loops@hotmail.com>
2019-02-20 22:47:09 +0100

    adding JSON data file, adding new styles for container, adding sass-loader and node-sass

--- pages/amsterdam/index.vue
+++ pages/amsterdam/index.vue
@@ -6,4 +6,13 @@ export default {}
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 150px;
+  margin: 0 auto;
+  padding: 150px;
+  min-height: 100vh;
+}
 </style>
--- pages/capeTown/index.vue
+++ pages/capeTown/index.vue
@@ -1,9 +1,18 @@
 <template>
-  <section class="container"/>
+  <section class="container" />
 </template>
 <script>
 export default {}
 </script>
 
 <style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 150px;
+  margin: 0 auto;
+  padding: 150px;
+  min-height: 100vh;
+}
 </style>
