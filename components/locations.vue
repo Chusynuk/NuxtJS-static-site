fa47e4037f92272a4fa008dbcbacf954983d5d27
Chusynuk <recycled_loops@hotmail.com>
2019-02-21 12:59:07 +0100

    adding new location component

---
+++ components/locations.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="locations">
+    <nuxt-link
+      to="/"
+      class="city"
+      name="berlin"
+    >
+      All
+    </nuxt-link>
+    <nuxt-link
+      to="/berlin"
+      class="city"
+      name="berlin"
+    >
+      Berlin
+    </nuxt-link>
+    <nuxt-link
+      to="/amsterdam"
+      class="city"
+      name="amsterdam"
+    >
+      Amsterdam
+    </nuxt-link>
+    <nuxt-link
+      to="/capetown"
+      class="city"
+      name="capeTown"
+    >
+      Cape Town
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Locations'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/index.scss";
+
+.locations {
+  display: flex;
+  justify-content: space-around;
+  text-align: center;
+  width: 50vw;
+  border: 2px solid $primary-yellow;
+  border-radius: 15px;
+  margin-top: 100px;
+}
+
+.city {
+  width: stretch; // OR fill-available
+  color: $black;
+  text-decoration: none;
+}
+</style>
